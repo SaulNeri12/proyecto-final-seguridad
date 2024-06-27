37aaf7a57556e0e127e043d4c7b44b71c451b29e
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-06-27 20:46:20 +0000

    ...

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 # configura la URL de la base de datos (en este caso MySQL)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://neri6502:cryptocucumber6502@neri6502.mysql.pythonanywhere-services.com/neri6502$proyecto-final"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['SQLALCHEMY_POOL_SIZE'] = 10 # conexiones a base de datos simultaneas
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3600  # 1 hora maximo por conexion
 
 # instancia del cursor de la base de datos
 db = SQLAlchemy(app)
