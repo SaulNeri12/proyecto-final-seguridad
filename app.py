f74b9933d28fbfbf2f83400a2cbea7959e9151d2
Saul Neri <saul.neri247498@potros.itson.edu.mx>
2024-06-30 08:09:55 +0000

    ya se muestra el captcha correctamente en el formulario de registro, y el formulario ya tiene la estructura de validacion definida con FlaskWTF y WTForms. Falta terminar de hacer la funcionalidad de registro (anadir los datos a un modelo del ORM y registrarlo en la base de datos) para realizar el login...

--- app.py
+++ app.py
@@ -7,20 +7,57 @@ from flask import Flask
 from flask_talisman import Talisman, ALLOW_FROM
 # Servicio de ORM para Flask
 from flask_sqlalchemy import SQLAlchemy
+# carga la libreria para acceder al archivo de variables de la aplicacion
+from dotenv import load_dotenv
+
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+# se carga el archivo de variables de la aplicacion...
+load_dotenv(os.path.join(BASEDIR, '.env'))
+
 
 app = Flask(__name__)
 
 # configura la URL de la base de datos (en este caso MySQL)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://neri6502:cryptocucumber6502@neri6502.mysql.pythonanywhere-services.com/neri6502$proyecto-final"
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqldb://neri6502:cryptocucumber6502@neri6502.mysql.pythonanywhere-services.com/neri6502$proyecto-final' 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['SQLALCHEMY_POOL_SIZE'] = 10 # conexiones a base de datos simultaneas
 app.config['SQLALCHEMY_POOL_TIMEOUT'] = 3600  # 1 hora maximo por conexion
 app.config['WTF_CSRF_ENABLED'] = True
 
+# se carga la clave secreta de la aplicacion
+app.config['SECRET_KEY'] = os.getenv("APP_SECRET_KEY")
+
+# configuracion del captcha
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('RECAPTCHA_PUBLIC_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('RECAPTCHA_PRIVATE_KEY')
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+
 # instancia del cursor de la base de datos
 db = SQLAlchemy(app)
 
+# Configuración de CSP para permitir scripts de 'self' y 'https://www.google.com'
+# permite que se inserte codigo JavaScript de estas fuentes solamente, fuentes
+# las cuales son "confiables" para nuestra aplicacion ya que nos permiten que se
+# muestre el reCAPTCHA
+csp = {
+    'default-src': [
+        '\'self\''
+    ],
+    'script-src': [
+        '\'self\'',
+        'https://www.google.com',
+        'https://www.gstatic.com'
+    ],
+    'frame-src': [
+        '\'self\'',
+        'https://www.google.com'
+    ]
+}
 
+Talisman(app, content_security_policy=csp)
 
 # Talisman para definir comportamientos especificos en cada ruta...
 talisman = Talisman(app)
--- views.py
+++ views.py
@@ -3,6 +3,57 @@ from flask import render_template, request
 from models import User, register_user
 from app import app
 
+from flask_wtf import FlaskForm, RecaptchaField
+from wtforms import StringField, EmailField, PasswordField
+from wtforms.validators import Email, Length, InputRequired
+
+print("[*] Flask WTF funcionando!!!")
+
+class SignupForm(FlaskForm):
+    username = StringField('Nombre de usuario', 
+        [
+            InputRequired(), 
+            Length(
+                min=8, 
+                max=32, 
+                message='El nombre de usuario debe tener al menos 8 caracteres y no pasar de 32 caracteres'
+            )
+        ]
+    )
+    email = EmailField('Correo electronico', 
+        [
+            InputRequired(),
+            Email(message='El correo electronico dado es incorrecto'),
+            Length(
+                max=320,
+                message='El correo electronico dado es demasiado largo'
+            )
+        ]
+    )
+    password = PasswordField('Contrasena',
+        [
+            InputRequired(),
+            Length(
+                min=8,
+                max=32,
+                message='La contrasena debe contener al menos 8 caracteres y maximo de 32'
+            )
+        ]
+    )
+    confirm_password = PasswordField('Confirmar Contrasena',
+        [
+            InputRequired(),
+            Length(
+                min=8,
+                max=32,
+                message='La contrasena debe contener al menos 8 caracteres y maximo de 32'
+            ),
+        ]    
+    )
+    recaptcha = RecaptchaField()
+
+
+
 @app.route('/')
 @app.route('/home')
 @app.route('/inicio')
@@ -27,11 +78,13 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     #register_user("pedro", "pedro123@gmail,com", "shesh")
-    
+
+    form = SignupForm()
+
     if request.method == "POST":
         print("POST SIGNUP!!!!")
 
-    return render_template('signup.html')
+    return render_template('signup.html', form=form)
 
 
 @app.route('/test-create-user', methods=['GET'])
